48e554bf8ae679c72c8f11895f546406d4ee91ab
finnole12 <finnolefinnole@yahoo.de>
2022-11-30 14:46:05 +0100

    adds bad input rejection

--- index.js
+++ index.js
@@ -7,27 +7,77 @@ app.get('/getRestaurants', (req, res) => {
     console.log(req)
     const latitude = req.query.latitude
     const longitude = req.query.longitude
-    const searchTerm = req.query.searchTerm
-    const size = req.query.size
-    const offset = req.query.offset
-    const radius = req.query.radius
+    const searchTerm = req.query.searchTerm ? req.query.searchTerm : ""
+    const radius = req.query.radius ? req.query.radius : 1000
+    const limit = req.query.limit ? req.query.limit : 20
+    const offset = req.query.offset ? req.query.offset : 0
+    const sortBy = req.query.sortBy ? req.query.sortBy : "distance"
 
-    if (!checkInput(latitude, longitude, searchTerm)) res.status(400).send({
-        status: 400,
-        message: "input missing" 
-    })
+    rejectBadInput(res, longitude, latitude, radius, limit, offset, sortBy)
 
-    res.send(searchDB())
+    res.send(searchDB(latitude, longitude))
 })
 
 app.listen(port, () => {
     console.log("now listening")
 })
 
-function checkInput(latitude, longitude) {
-    return latitude && longitude
+function rejectBadInput(res, longitude, latitude, radius, limit, offset, sortBy) {
+
+    // Missing Input
+    if (!longitude || !latitude) res.status(400).send({
+        status: 400,
+        message: "Missing Input: longitude and latitude are required" 
+    })
+
+    // Wrong Type
+    if (isNaN(longitude) || isNaN(latitude)) res.status(400).send({
+        status: 400,
+        message: "Wrong Type: longitude and latitude must be numbers" 
+    })
+    if (isNaN(radius)) res.status(400).send({
+        status: 400,
+        message: "Wrong Type: radius must be a number" 
+    })
+    if (isNaN(limit)) res.status(400).send({
+        status: 400,
+        message: "Wrong Type: limit must be a number" 
+    })
+    if (isNaN(offset)) res.status(400).send({
+        status: 400,
+        message: "Wrong Type: offset must be a number" 
+    })
+
+    // Wrong Values
+    if (limit < 0 || limit > 50) res.status(400).send({
+        status: 400,
+        message: "Wrong Value: limit must be in range 0..50"
+    })
+    if (radius < 0) res.status(400).send({
+        status: 400,
+        message: "Wrong Value: radius must be positive"
+    })
+    if (!["distance", "rating", "relevancy"].includes(sortBy.toLowerCase())) res.status(400).send({
+        status: 400,
+        message: "Wrong Value: sortBy must be distance, rating or relevancy"
+    })
+    if (offset < 0) res.status(400).send({
+        status: 400,
+        message: "Wrong Value: offset must be positive"
+    })
 }
 
 function searchDB(latitude, longitude, searchTerm = "all", radius = 1000) {
-    return data
+    // checken nach latitude longitude simple
+    var fistBatch = data.filter((entry) => {
+        return Math.abs(entry.latitude - latitude) <= 1000 && Math.abs(entry.longitude - longitude) <= 1000
+    })
+
+    var legitSpots = fistBatch.filter((entry) => {
+        var a = Math.abs(entry.latitude - latitude)
+        var b = Math.abs(entry.longitude - longitude)
+        var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+        return c <= 1000
+    })
+    return legitSpots
 }
